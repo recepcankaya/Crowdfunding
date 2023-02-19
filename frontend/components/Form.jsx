fb413286cd650da0e7cd7237955c5d759b411ef4
Recep Cankaya <recepcnky@outlook.com>
2023-02-19 22:13:30 +0300

    Add validation for the form

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import { useFormik } from "formik";
+import { object, string, number } from "yup";
 
 export default function Form() {
   const formik = useFormik({
@@ -7,6 +7,16 @@ export default function Form() {
       description: "",
       requestedContribution: "",
     },
+    validationSchema: object({
+      description: string()
+        .min(10, "Must be at least 10 characters")
+        .max(100, "Must be 100 character or less!")
+        .required("This field is required!"),
+      requestedContribution: number()
+        .max(18, "Must be 18 character or less!")
+        .required("This field is required!")
+        .positive(),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -29,7 +39,13 @@ export default function Form() {
             value={formik.values.description}
             className="w-1/2 h-8 mt-2 pl-2 border-solid border border-orange-600 rounded-md focus:outline-0"
           />
+          {formik.touched.description && formik.errors.description ? (
+            <div className="w-1/2 mt-2 bg-red-600 rounded-lg text-white">
+              <span className="p-2">{formik.errors.description}</span>
+            </div>
+          ) : null}
         </div>
+
         <div className="mb-6">
           <label htmlFor="requestedContribution">
             Please enter the requested amount for your project
@@ -42,6 +58,12 @@ export default function Form() {
             value={formik.values.requestedContribution}
             className="w-1/2 h-8 mt-2 pl-2 border-solid border border-orange-600 rounded-md focus:outline-0"
           />
+          {formik.touched.requestedContribution &&
+          formik.errors.requestedContribution ? (
+            <div className="w-1/2 mt-2 bg-red-600 rounded-lg text-white">
+              <span className="p-2">{formik.errors.requestedContribution}</span>
+            </div>
+          ) : null}
         </div>
         <button className="w-1/4 mt-4 border-solid border border-violet-800 rounded-lg hover:bg-gradient-to-br from-orange-600 to-violet-700 hover:text-white">
           Submit
