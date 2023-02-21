c56b288abfa738e65526e14256b90df8b62ee076
Recep Cankaya <recepcnky@outlook.com>
2023-02-21 15:06:02 +0300

    Pass the test 'Should vote on proposal'

--- hardhat-side/test/Fund.test.js
+++ hardhat-side/test/Fund.test.js
@@ -7,6 +7,7 @@ describe("Fund", function () {
   beforeEach(async function () {
     fundContractFactory = await ethers.getContractFactory("Fund");
     fundContract = await fundContractFactory.deploy();
+    await fundContract.deployed();
   });
 
   describe("Create Proposal", function () {
@@ -17,7 +18,9 @@ describe("Fund", function () {
     });
 
     it("Should update propsalId by one after proposal created", async function () {
-      const txResponse = await fundContract.createProposal("This is a proposal");
+      const txResponse = await fundContract.createProposal(
+        "This is a proposal", 0
+      );
       await txResponse.wait(1);
       const expectedId = "1";
       const proposalId = await fundContract.proposalId();
@@ -25,14 +28,33 @@ describe("Fund", function () {
     });
 
     it("Should create the proposal and get it from the mapping with the given description", async function () {
-      const firstProposal = await fundContract.createProposal("This is the first proposal");
+      const firstProposal = await fundContract.createProposal(
+        "This is the first proposal", 0
+      );
       await firstProposal.wait(1);
-      const secondProposal = await fundContract.createProposal("This is the second proposal");
+      const secondProposal = await fundContract.createProposal(
+        "This is the second proposal", 0
+      );
       await secondProposal.wait(1);
       const proposal1 = await fundContract.proposals(0);
       const proposal2 = await fundContract.proposals(1);
       assert.equal(proposal1.description, "This is the first proposal");
       assert.equal(proposal2.description, "This is the second proposal");
+    });
+  });
+
+  describe("Vote Proposal", function () {
+    const vote = {
+      NO: 0,
+      YES: 1,
+    };
+
+    it.only("Should vote on proposal", async function () {
+      const theProposal = await fundContract.createProposal("Test proposal", 0);
+      theProposal.wait(1);
+      await fundContract.voteProposal(0, vote.YES);
+      const proposal = await fundContract.proposals(0);
+      assert.equal(proposal.yes.toString(), "1");
     });
   });
 });
